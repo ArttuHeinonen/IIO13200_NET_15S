03bd5b482aa81c803b51d1909982daf191b14962
ArttuHeinonen <arttu.hei@gmail.com>
2015-09-24 15:33:10 +0300

    Tehtava 5 aloitus
    Kaikki läsnäolot näkyy

--- ADODataReader/Program.cs
+++ ADODataReader/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,82 @@ namespace ADODataReader
     {
         static void Main(string[] args)
         {
+            LubricateConsole();
+            //GetData_DataReader();
+            GetData_DataTable();
+        }
+        static void LubricateConsole()
+        {
             Console.Title = "ADODataReader";
             Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        static void GetData_DataTable()
+        {
+            //UI-kerros data esittämistä varten
+            try
+            {
+                //Heataan data DataTablen avulla
+                DataTable dt = GetDataReal();
+                string rivi = "";
+                //Loopitetaan DataTablen rivin läpi
+                foreach (DataRow dr in dt.Rows)
+                {
+                    foreach (DataColumn dc in dt.Columns)
+                    {
+                        rivi += dr[dc].ToString() + " ";
+                    }
+                    Console.WriteLine(rivi);
+                    rivi = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static DataTable GetDataSimple()
+        {
+            DataTable dt = new DataTable();
+            //Sarakkeet
+            dt.Columns.Add("FirstName", typeof(string));
+            dt.Columns.Add("LastName", typeof(string));
+            //Rivit
+            dt.Rows.Add("Kalle", "Isokallio");
+            dt.Rows.Add("Matt", "Majula");
+
+            return dt;
+        }
+
+        static DataTable GetDataReal()
+        {
+            //DBkerros, haetaan DemoxOy-tietokannasta taulun lasnaolot tietueet, palauttaa DataTablen
+            string sql = "SELECT asioid, lastname, firstname, date FROM lasnaolot";// WHERE asioid='H3425'";
+            string connStr = @"Data source=eight.labranet.jamk.fi;initial catalog=DemoxOy;user=koodari;password=koodari13";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    //Avataan yhteys
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataSet ds = new DataSet();
+                        da.Fill(ds, "lasnaolot");
+                        conn.Close();
+                        return ds.Tables["lasnaolot"];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        static void GetData_DataReader()
+        {     
             try
             {
                 string sql = "SELECT asioid, lastname, firstname, date FROM lasnaolot WHERE asioid='H3425'";
---
+++ Tehtava5/DBDemoxOy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tehtava5
+{
+    public static class DBDemoxOy
+    {
+
+
+
+        public static DataTable GetDataSimple()
+        {
+            DataTable dt = new DataTable();
+            //Sarakkeet
+            dt.Columns.Add("FirstName", typeof(string));
+            dt.Columns.Add("LastName", typeof(string));
+            //Rivit
+            dt.Rows.Add("Kalle", "Isokallio");
+            dt.Rows.Add("Matt", "Majula");
+
+            return dt;
+        }
+
+        public static DataTable GetDataReal()
+        {
+            //DBkerros, haetaan DemoxOy-tietokannasta taulun lasnaolot tietueet, palauttaa DataTablen
+            string sql = "SELECT asioid, lastname, firstname, date FROM lasnaolot";// WHERE asioid='H3425'";
+            string connStr = Tehtava5.Properties.Settings.Default.connection.ToString();
+            //string connStr = @"Data source=eight.labranet.jamk.fi;initial catalog=DemoxOy;user=koodari;password=koodari13";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    //Avataan yhteys
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataSet ds = new DataSet();
+                        da.Fill(ds, "lasnaolot");
+                        conn.Close();
+                        return ds.Tables["lasnaolot"];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
