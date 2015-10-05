e6d8030e06367403077ac2460643f233f28dbd93
ArttuHeinonen <arttu.hei@gmail.com>
2015-10-05 19:23:41 +0300

    Harkkatyo aloitus
    oh damn nyt jo

---
+++ Harkka/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harkka
+{
+    public class Resource
+    {
+        string name { get; set; }
+        float value { get; set; }
+    }
+}
